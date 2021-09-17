f5fd60e39263ee05a69a8542c470fb6f09042251
bwill322 <74652352+bwill322@users.noreply.github.com>
2021-09-17 13:00:44 -0400

    changes to allow Catalyst and ISR platforms to work

--- interface-errors.py
+++ interface-errors.py
@@ -21,9 +21,15 @@ net_connect = Netmiko(**device)
 
 output = net_connect.send_command_timing("show interfaces", use_textfsm=True)
 
-print("{:25}{:<15}{:<15}{:<15}".format("Interface Name", "Input Errors", "Output Errors", "CRC Errors"))
-print("-" * 80)
-for i in range(len(output)):
+print("-" * 160)
+print("{:35} | {:<15} | {:<15} | {:<15} | {:<25}".format("Interface Name",
+                                                         "Input Errors",
+                                                         "Output Errors",
+                                                         "CRC Errors",
+                                                         "Description")
+      )
+print("-" * 160)
+for i, element in enumerate(output):
     hardware_type = output[i]['hardware_type']
 
     if "Ethernet SVI" in hardware_type:
@@ -32,11 +38,36 @@ for i in range(len(output)):
         continue
     else:
         intf_name = output[i]['interface']
-        input_errors = int(output[i]['input_errors'])
-        output_errors = int(output[i]['output_errors'])
-        crc_errors = int(output[i]['crc'])
+        input_errors = output[i]['input_errors']
+        output_errors = output[i]['output_errors']
+        crc_errors = output[i]['crc']
+        description = output[i]['description']
+
+        # To account for interfaces that don't have these counters (e.g. subintfs)
+        if input_errors == '':
+            input_errors = 0
+        else:
+            input_errors = int(input_errors)
+
+        if output_errors == '':
+            output_errors = 0
+        else:
+            output_errors = int(output_errors)
+
+        if crc_errors == '':
+            crc_errors = 0
+        else:
+            crc_errors = int(crc_errors)
 
         intf_has_errors = input_errors > 0 or output_errors > 0 or crc_errors > 0
 
         if intf_has_errors:
-            print("{:25}{:<15}{:<15}{:<15}".format(intf_name, input_errors, output_errors, crc_errors))
+            print("{:35} | {:<15} | {:<15} | {:<15} | {:<25}".format(
+                intf_name,
+                input_errors,
+                output_errors,
+                crc_errors,
+                description)
+            )
+
+print()
