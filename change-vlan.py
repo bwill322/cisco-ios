ed35359fddebcd6a8f44a662c8f2e2de6521d2dd
bwill322 <74652352+bwill322@users.noreply.github.com>
2021-09-24 13:08:27 -0400

    add login_tools.py, adjust scripts to use it

--- change-vlan.py
+++ change-vlan.py
@@ -5,8 +5,11 @@ Update an interface and change the macro applied to it
 
 from __future__ import print_function, unicode_literals
 import os
+import signal
 from netmiko import Netmiko
-from getpass import getpass
+import login_tools
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)  # KeyboardInterrupt: Ctrl-C
 
 
 def make_changes(data):
@@ -36,66 +39,71 @@ def make_changes(data):
         print("-" * 80)
 
 
-hostname = input("Enter hostname: ")
-username = input("Enter username: ")
+netmiko_exceptions = (netmiko.ssh_exception.NetmikoAuthenticationException,
+                      netmiko.ssh_exception.NetmikoTimeoutException)
+
+hostname = login_tools.get_input("Enter hostname: ")
+username, password = login_tools.get_credentials()
 port_descr = input("Port Description: ")
 new_macro = input("Macro to Apply: ")
 
 device = {
     'host': hostname,
     'username': username,
-    'password': getpass(),
+    'password': password,
     'device_type': 'cisco_ios'
 }
+try:
+    net_connect = Netmiko(**device)
 
-net_connect = Netmiko(**device)
+    output = net_connect.send_command_timing("show interfaces", use_textfsm=True)
 
-output = net_connect.send_command_timing("show interfaces", use_textfsm=True)
-
-print()
-print("-" * 80)
-print("{:<30}{:<30}".format(
-    "Interface Name",
-    "Description"
+    print()
+    print("-" * 80)
+    print("{:<30}{:<30}".format(
+        "Interface Name",
+        "Description"
+        )
     )
-)
-print("-" * 80)
+    print("-" * 80)
 
-for i in range(len(output)):
-    intf_name = output[i]['interface']
-    description = output[i]['description']
+    for i in range(len(output)):
+        intf_name = output[i]['interface']
+        description = output[i]['description']
 
-    if port_descr in description:
-        print("{:<30}{:<30}".format(intf_name, description))
-        command = "show running-config interface " + intf_name
-        intf_running_config = net_connect.send_command_timing(command)
-        print()
-        print("-" * 80)
-        print("Current Interface Config")
-        print("-" * 80)
-        print(intf_running_config)
-
-        config_changes = [
-            'default interface ' + intf_name,
-            'interface ' + intf_name,
-            'description ' + description,
-            'macro apply ' + new_macro
-        ]
-
-        proceed = input("Proceed with changes? [y/n]: ")
-
-        if 'y' in proceed:
-            make_changes(config_changes)
-
-            print()
-            print("-" * 80)
-            print("New Interface Config....")
-            print("-" * 80)
+        if port_descr in description:
+            print("{:<30}{:<30}".format(intf_name, description))
+            command = "show running-config interface " + intf_name
             intf_running_config = net_connect.send_command_timing(command)
-            print(intf_running_config)
-            break
-        else:
             print()
             print("-" * 80)
-            print("Exiting out, making no changes")
+            print("Current Interface Config")
             print("-" * 80)
+            print(intf_running_config)
+
+            config_changes = [
+                'default interface ' + intf_name,
+                'interface ' + intf_name,
+                'description ' + description,
+                'macro apply ' + new_macro
+            ]
+
+            proceed = input("Proceed with changes? [y/n]: ")
+
+            if 'y' in proceed:
+                make_changes(config_changes)
+
+                print()
+                print("-" * 80)
+                print("New Interface Config....")
+                print("-" * 80)
+                intf_running_config = net_connect.send_command_timing(command)
+                print(intf_running_config)
+                break
+            else:
+                print()
+                print("-" * 80)
+                print("Exiting out, making no changes")
+                print("-" * 80)
+except netmiko_exceptions as e:
+    print("Failed to: ", hostname, e)
--- interface_errors.py
+++ interface_errors.py
@@ -3,67 +3,75 @@ Look for interfaces with errors
 """
 
 from __future__ import print_function, unicode_literals
-
-from getpass import getpass
+import signal
+import netmiko.ssh_exception
 from netmiko import Netmiko
+import login_tools
 
-hostname = input("Enter hostname: ")
-username = input("Enter username: ")
+signal.signal(signal.SIGINT, signal.SIG_DFL)  # KeyboardInterrupt: Ctrl-C
+
+hostname = login_tools.get_input("Enter hostname: ")
+username, password = login_tools.get_credentials()
 
 device = {
     'host': hostname,
     'username': username,
-    'password': getpass(),
+    'password': password,
     'device_type': 'cisco_ios'
 }
 
-net_connect = Netmiko(**device)
+netmiko_exceptions = (netmiko.ssh_exception.NetmikoAuthenticationException,
+                      netmiko.ssh_exception.NetmikoTimeoutException)
 
-output = net_connect.send_command_timing("show interfaces", use_textfsm=True)
+try:
+    net_connect = Netmiko(**device)
+    output = net_connect.send_command_timing("show interfaces", use_textfsm=True)
 
-print("-" * 160)
-print("{:35} | {:<15} | {:<15} | {:<15} | {:<25}".format("Interface Name",
-                                                         "Input Errors",
-                                                         "Output Errors",
-                                                         "CRC Errors",
-                                                         "Description")
-      )
-print("-" * 160)
-for i, element in enumerate(output):
-    hardware_type = output[i]['hardware_type']
+    print("-" * 160)
+    print("{:35} | {:<15} | {:<15} | {:<15} | {:<25}".format("Interface Name",
+                                                             "Input Errors",
+                                                             "Output Errors",
+                                                             "CRC Errors",
+                                                             "Description")
+          )
+    print("-" * 160)
+    for i, element in enumerate(output):
+        hardware_type = output[i]['hardware_type']
 
-    if "Ethernet SVI" not in hardware_type or "RP Management Port" not in hardware_type:
-        intf_name = output[i]['interface']
-        input_errors = output[i]['input_errors']
-        output_errors = output[i]['output_errors']
-        crc_errors = output[i]['crc']
-        description = output[i]['description']
+        if "Ethernet SVI" not in hardware_type or "RP Management Port" not in hardware_type:
+            intf_name = output[i]['interface']
+            input_errors = output[i]['input_errors']
+            output_errors = output[i]['output_errors']
+            crc_errors = output[i]['crc']
+            description = output[i]['description']
 
-        # To account for interfaces that don't have these counters (e.g. subintfs)
-        if input_errors == '':
-            input_errors = 0
-        else:
-            input_errors = int(input_errors)
+            # To account for interfaces that don't have these counters (e.g. subintfs)
+            if input_errors == '':
+                input_errors = 0
+            else:
+                input_errors = int(input_errors)
 
-        if output_errors == '':
-            output_errors = 0
-        else:
-            output_errors = int(output_errors)
+            if output_errors == '':
+                output_errors = 0
+            else:
+                output_errors = int(output_errors)
 
-        if crc_errors == '':
-            crc_errors = 0
-        else:
-            crc_errors = int(crc_errors)
+            if crc_errors == '':
+                crc_errors = 0
+            else:
+                crc_errors = int(crc_errors)
 
-        intf_has_errors = input_errors > 0 or output_errors > 0 or crc_errors > 0
+            intf_has_errors = input_errors > 0 or output_errors > 0 or crc_errors > 0
 
-        if intf_has_errors:
-            print("{:35} | {:<15} | {:<15} | {:<15} | {:<25}".format(
-                intf_name,
-                input_errors,
-                output_errors,
-                crc_errors,
-                description)
-            )
+            if intf_has_errors:
+                print("{:35} | {:<15} | {:<15} | {:<15} | {:<25}".format(
+                    intf_name,
+                    input_errors,
+                    output_errors,
+                    crc_errors,
+                    description)
+                )
 
-print()
+    print()
+except netmiko_exceptions as e:
+    print("Failed to: ", hostname, e)
